83a84e748444d5c7bc8ef0c1de8514ae90410c32
xuges <xuges@xugtek.com>
2026-08-17 20:10:46 +0800

    [token_calc] Align tool site visual style with xugtek.com

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,7 +1,13 @@
 import { initI18n } from './i18n.js';
 import { initTheme } from './theme.js';
 
+function initYear() {
+  const yearEl = document.getElementById('year');
+  if (yearEl) yearEl.textContent = String(new Date().getFullYear());
+}
+
 export function initSite() {
   initTheme();
   initI18n();
+  initYear();
 }
